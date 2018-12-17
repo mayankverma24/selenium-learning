722edc6446cb3b1d6ae99258e125475b9dbffc66
mayankverma786 <mayankverma786@gmail.com>
2018-12-18 00:58:24 +0530

    Changed Folder Structure created separate fodlers for drivers and selenium/poi library

--- src/JavaBasics/Basics1.java
+++ src/JavaBasics/Basics1.java
@@ -3,7 +3,6 @@ package JavaBasics;
 public class Basics1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		//This step prints in the output
 		System.out.println("I started Java learning.");
@@ -11,7 +10,7 @@ public class Basics1 {
 		//Add 2 numbers
 		int a=2;
 		int b=5;
-		int c=a*b;
+		int c=a+b;
 		System.out.println("Sum is "+c);
 	}
 		//3 lines of code header
--- src/selenium/csslocators.java
+++ src/selenium/csslocators.java
@@ -9,12 +9,13 @@ public class csslocators {
 	public static void main(String[] args) {
 		//System.out.println("Hello mn");
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mayan\\eclipse-workspace\\selenium-learning\\chromedriver.exe");
+		String username = System.getenv("USERNAME");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\"+ username + "\\eclipse-workspace\\selenium-learning\\resources\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.cleartrip.com/");
 		
-		//-Creating custom xpath //tagname[@attribute='value']
+		//-Creating custom xpath: //tagname[@attribute='value']
 		driver.findElement(By.xpath("//input[@etitle='From']")).sendKeys("BOM");
 		
 		/*-Creating custom css
@@ -27,7 +28,12 @@ public class csslocators {
 		//Print data from web element
 		System.out.println(driver.findElement(By.cssSelector("#flightsHomeFlashSaleBanner")).getText());
 		
-		driver.quit();
+		//xpath regular expression: //tagname[contains(@attribute,'value')]
+		driver.findElement(By.xpath("//div[@id='ORtrip']/section[1]/div[1]/dl/dd/span/input[contains(@etitle,'Fro')]")).sendKeys("IDR");
+
+		//CSS regular expression: //tagname[attribute*='value']
+		//driver.findElement(By.cssSelector("//div[@id='ORtrip']/section[1]/div[1]/dl/dd/span/input[etitle*='Fro']")).sendKeys("BHO");
+		//driver.quit();
 		
 		
 	}
